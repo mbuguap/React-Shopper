1462e9289dc928ba2c1ba1b74fd273692980701b
mbuguap <petrmbugua@gmail.com>
2021-10-31 15:48:29 +0300

    complete app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import Result from './pages/Result';
+import { loadStripe } from '@stripe/stripe-js';
+import { CartProvider } from 'use-shopping-cart';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/Navbar';
+
+const queryClient = new QueryClient();
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_CLIENT_KEY);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <CartProvider
+        mode='checkout-session'
+        stripe={stripePromise}
+        currency='USD'
+      >
+        <BrowserRouter>
+          <Navbar />
+          <Toaster position='bottom-center' />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/result' component={Result} />
+            <Route path='/:productId' component={Product} />
+          </Switch>
+        </BrowserRouter>
+      </CartProvider>
+    </QueryClientProvider>
   );
 }
 
